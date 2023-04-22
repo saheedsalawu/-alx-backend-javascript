0d4b0e981177bde558d622662ed49d293b9afc82
saheedsalawu <106780350+saheedsalawu@users.noreply.github.com>
2023-04-21 20:44:22 -0900

    Update 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -3,12 +3,10 @@ import Car from './10-car';
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this.range = range;
+    this._range = range;
   }
 
-  set range(value) { this._range = value; }
-
-  get range() { return this._range; }
-
-  static get [Symbol.species]() { return Car; }
+  static get [Symbol.species]() {
+    return Car;
+  }
 }
