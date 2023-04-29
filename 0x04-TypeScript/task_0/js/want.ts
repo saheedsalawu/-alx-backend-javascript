2476eaaf50cf85ddd36df44e04ce08053d201b89
saheedsalawu <106780350+saheedsalawu@users.noreply.github.com>
2023-04-29 03:49:09 +0100

    Create want.ts

---
+++ 0x04-TypeScript/task_0/js/want.ts
@@ -0,0 +1,26 @@
+interface Student {
+  firstName: string,
+  lastName: string,
+  age: number,
+  location: string
+}
+
+const [student1, student2] = [
+  {
+    firstName: "Mark",
+    lastName,: "Oshorenoya"
+    age: 25,
+    location: "Lagos State"
+  },
+  {
+    firstName: "Marcus",
+    lastName: "Imagwe",
+    age: 33,
+    location: "Port Harcourt"
+  },
+]
+
+const studentsList : ñ
+
+table.append(tbody);
+table.append(table);
