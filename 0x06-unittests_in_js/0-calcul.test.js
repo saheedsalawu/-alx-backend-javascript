1dcfe6430eefaff01e739c5558f0a56102bbe1ac
saheedsalawu <106780350+saheedsalawu@users.noreply.github.com>
2023-06-15 14:05:17 -0900

    Update 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,22 +1,49 @@
-const calculateNumber = require("./0-calcul.js");
-const assert = require('assert');
+cconst assert = require('assert');
+const isEqual = assert.strictEqual;
+const calculateNumber = require('./0-calcul')
 
 describe('calculateNumber', () => {
-    it('rounding of a', () => {
-        assert.equal(calculateNumber(15.78, 2), 18);
-        assert.equal(calculateNumber(1.2, 0), 1);
-        assert.equal(calculateNumber(3.5, 2), 6);
-    });
+  it('should pass_0', () => {
+    isEqual(calculateNumber(1, 3), 4);
+  });
 
-    it('rounding of b', () => {
-        assert.equal(calculateNumber(2, 15.78), 18);
-        assert.equal(calculateNumber(0, 1.2), 1);
-        assert.equal(calculateNumber(2, 3.5), 6);
-    });
+  it('should pass_1', () => {
+    isEqual(calculateNumber(10, 3), 13);
+  });
 
-    it('suming of a and b', () => {
-        assert.equal(calculateNumber(15.78, 2.5), 19);
-        assert.equal(calculateNumber(1.2, 0.2), 1);
-        assert.equal(calculateNumber(3.5, 2.7), 7);
-    });
-})
+  it('should pass_2', () => {
+    isEqual(calculateNumber(1, 0), 1);
+  });
+
+  it('should pass_3', () => {
+    isEqual(calculateNumber(2.5, 2.5), 6);
+  });
+
+  it('should pass_4', () => {
+    isEqual(calculateNumber(0.1, 0.3), 0);
+  });
+
+  it('should pass_5', () => {
+    isEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('should pass_6', () => {
+    isEqual(calculateNumber(893390874874, 494788474298), 1388179349172);
+  });
+
+  it('should pass_7', () => {
+    isEqual(calculateNumber(0.1, 0.5), 1);
+  });
+
+  it('should pass_8', () => {
+    isEqual(calculateNumber(1e5, 3e5), 4e5);
+  });
+
+  it('should pass_9', () => {
+    isEqual(calculateNumber(96, 3), 99);
+  });
+
+  it('should pass_10', () => {
+    isEqual(calculateNumber(Infinity, Infinity), Infinity);
+  });
+});
