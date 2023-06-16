44e0c52abd81cbc5c3a0c09a782bf772b29d143d
saheedsalawu <106780350+saheedsalawu@users.noreply.github.com>
2023-06-16 08:51:32 -0900

    Update 3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,19 +1,16 @@
-const sinon = require('sinon');
-const payment = require('./3-payment');
-const sendPaymentRequestToApi = payment.sendPaymentRequestToApi;
-const Utils = payment.Utils;
-const chai = require('chai');
-const expect = chai.expect;
+const {describe, it} = require("mocha");
+const sinon = require("sinon");
+const sendPaymentRequestToApi = require("./3-payment");
+const Utils = require("./utils");
+const assert = require("assert");
 
-describe('sendPaymentRequestToApi', () => {
-  it('Validates correct usage of Utils.CalculateNumber', () => {
-    sinon.spy(Utils, 'calculateNumber');
+describe("sendPaymentRequestToApi", function() {
+    it("check that Utils.calculateNumber was called once", function() {
+	const spy = sinon.spy(Utils, "calculateNumber");
 
-    sendPaymentRequestToApi(100, 20);
-    expect(Utils.calculateNumber.calledOnce).to.be.true;
-    expect(Utils.calculateNumber.firstCall.args[0]).to.equal('SUM');
-    expect(Utils.calculateNumber.firstCall.args[1]).to.equal(100);
-    expect(Utils.calculateNumber.firstCall.args[2]).to.equal(20);
-    Utils.calculateNumber.restore();
-  });
+	sendPaymentRequestToApi(50, 24.52);
+
+	assert(spy.calledOnce);
+	spy.restore();
+    });
 });
