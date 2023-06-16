557ac672a8c7f3fac1e2af391616eada2d12aa43
saheedsalawu <106780350+saheedsalawu@users.noreply.github.com>
2023-06-16 08:29:13 -0900

    Update 0-calcul.js

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,2 +1,7 @@
-const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+function calculateNumber(a, b) {
+  const anum = Math.round(a);
+  const bnum = Math.round(b);
+  return anum + bnum;
+}
+
 module.exports = calculateNumber;
