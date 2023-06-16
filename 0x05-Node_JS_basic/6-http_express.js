4ae0a4687b9d2a0fdcbb1506527002d3709e2694
saheedsalawu <106780350+saheedsalawu@users.noreply.github.com>
2023-06-16 14:42:34 -0900

    Update 6-http_express.js

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -2,13 +2,12 @@ const express = require('express');
 
 const app = express();
 const port = 1245;
-const host = '127.0.0.1';
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
 });
 
-
-app.listen(port, host);
+app.listen(port, () => {
+});
 
 module.exports = app;
