0e88536b02ebd52f8eba7a75d9e0ecb98be13d3c
saheedsalawu <106780350+saheedsalawu@users.noreply.github.com>
2023-06-16 08:58:27 -0900

    Update 4-payment.test.js

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,16 +1,16 @@
 const {describe, it} = require("mocha");
 const sinon = require("sinon");
-const sendPaymentRequestToApi = require("./4-payment");
+const sendPaymentRequestToApi = require("./3-payment");
 const Utils = require("./utils");
 const assert = require("assert");
 
 describe("sendPaymentRequestToApi", function() {
-    it("check that Utils.calculateNumber is stubbed", function() {
-	const spy = sinon.spy(console, "log");
-	const stub = sinon.stub(Utils, "calculateNumber").returns(10);
-	sendPaymentRequestToApi(100, 20);
+    it("check that Utils.calculateNumber was called once", function() {
+	const spy = sinon.spy(Utils, "calculateNumber");
 
-	assert(spy.withArgs("The total is: 10").calledOnce);
-	assert(stub.withArgs("SUM", 100, 20).calledOnce);
+	sendPaymentRequestToApi(50, 24.52);
+
+	assert(spy.calledOnce);
+	spy.restore();
     });
 });
